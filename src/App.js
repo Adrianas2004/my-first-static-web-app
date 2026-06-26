7e694d196d8348c8f5ab1f480da6ba636b3c8481
Adrianas2004 <adrianj2464@gmail.com>
2026-06-26 14:23:17 -0600

    Agregando integrantes del equipo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+
+  return (
+    <div>
+      <h1>Hello {value}</h1>
+
+      <h2>Integrantes del equipo</h2>
+
+      <ul>
+        <li>Tu nombre completo</li>
+        <li>Integrante 2</li>
+        <li>Integrante 3</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
